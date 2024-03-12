0746c705677ffc024f61b2d9f84713a09956520d
coleherman43 <colesten@gmail.com>
2024-03-12 10:40:16 -0700

    Made skeleton for Prims Algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,20 @@
 from graph import *
+import math
 
-
+def prims_algo(g: ConnectedGraph):
+    """pick lowestweight edge, then loop through neighbors and choose
+    lowest weight edge to unkown vertex, repeat"""
+    assert g.size > 0
+    assert len(g.points) > 0
+    # simply list of used vertices
+    vertices: list[Point] = []
+    # list of edges in (p1, p2, weight) form in order of adding. Will become path to return
+    path: list[tuple[Point,Point, int]] = []
+    # point to start with FIXME have to find minimum weight edge
+    
+    while len(vertices) < g.size:
+        pass
+    return path
 
 
 if __name__ == "__main__":
